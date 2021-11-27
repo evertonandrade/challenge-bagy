44afe628d8613bff3ec3f52fa52de7590d4ec7a0
Everton Andrade <evertonfa7@gmail.com>
2021-11-27 02:54:58 -0300

    create icon components

---
+++ src/ui/icons.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { ReactComponent as BookSvg } from '../assets/icons/book.svg'
+import { ReactComponent as ChartPieSvg } from '../assets/icons/chart-pie.svg'
+import { ReactComponent as DividerSvg } from '../assets/icons/divider.svg'
+import { ReactComponent as LightBulbSvg } from '../assets/icons/light-bulb.svg'
+import { ReactComponent as MedalSvg } from '../assets/icons/medal.svg'
+import { ReactComponent as NotificationSvg } from '../assets/icons/notification.svg'
+import { ReactComponent as SearchSvg } from '../assets/icons/search.svg'
+import { ReactComponent as SettingsSvg } from '../assets/icons/settings.svg'
+import { ReactComponent as TicketSvg } from '../assets/icons/ticket.svg'
+import { ReactComponent as UserTieSvg } from '../assets/icons/user-tie.svg'
+import { ReactComponent as UsersSvg } from '../assets/icons/users.svg'
+
+export const BookIcon = styled(BookSvg)``
+export const ChartPieIcon = styled(ChartPieSvg)``
+export const DividerIcon = styled(DividerSvg)``
+export const LightBulbIcon = styled(LightBulbSvg)``
+export const MedalIcon = styled(MedalSvg)``
+export const NotificationIcon = styled(NotificationSvg)``
+export const SearchIcon = styled(SearchSvg)``
+export const SettingsIcon = styled(SettingsSvg)``
+export const TicketIcon = styled(TicketSvg)``
+export const UserTieIcon = styled(UserTieSvg)``
+export const UsersIcon = styled(UsersSvg)``
