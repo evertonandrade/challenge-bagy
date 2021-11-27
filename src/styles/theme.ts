e78d7c2fd0a09830e70a71a3b003585f71ab0584
Everton Andrade <evertonfa7@gmail.com>
2021-11-27 02:51:30 -0300

    add colors in the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,15 @@
 import { DefaultTheme } from 'styled-components'
 
-export const theme: DefaultTheme = {}
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#000000',
+    lightBlack: '#363740',
+    gray: '#A4A6B3',
+    lightGray: '#C5C7CD',
+    white: '#FFFFFF',
+    pink: '#FC3C8D',
+    yellow: '#FEC400',
+    blue: '#2F80ED',
+    green: '#219653'
+  }
+}
