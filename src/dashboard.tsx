0e4ba76b8a544ae2195374705580af4566187937
Everton Andrade <evertonfa7@gmail.com>
2021-11-27 02:51:53 -0300

    create dashboard component

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components'
+import { Content } from './components/content'
+import { Sidenav } from './components/sidenav'
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  min-height: 100vh;
+`
+
 export function Dashboard() {
-  return <div>Hello React!</div>
+  return (
+    <Container>
+      <Sidenav />
+      <Content />
+    </Container>
+  )
 }
